b3e6cdeaa86276a850908b854329548d9f6336fb
Bilal <malixbilix786@gmail.com>
2022-08-09 16:11:15 -0700

    Updates

---
+++ precache-manifest.20fa6473a259ff1f42d9cd6720602192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be8cc597e74b381807c6",
+    "url": "/BilixUtils/static/css/main.2bd48032.chunk.css"
+  },
+  {
+    "revision": "be8cc597e74b381807c6",
+    "url": "/BilixUtils/static/js/main.be8cc597.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/BilixUtils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "680ca31805d1e340ae69",
+    "url": "/BilixUtils/static/js/2.680ca318.chunk.js"
+  },
+  {
+    "revision": "9acfb4d6834d25acd666",
+    "url": "/BilixUtils/static/js/runtime~main.9acfb4d6.js"
+  },
+  {
+    "revision": "ae7eb540ed3151ca53c1a27b0d0044c7",
+    "url": "/BilixUtils/index.html"
+  }
+];
